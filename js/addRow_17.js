576aa4c3852ed9ffdfce54b84a9fc2dfedc0cc62
codriv <67503884+codriv@users.noreply.github.com>
2023-02-19 18:41:58 +0100

    Update addRow_17.js
    headers_1

--- js/addRow_17.js
+++ js/addRow_17.js
@@ -16,7 +16,26 @@ var gombMod = '<button type="button" class="btn btn-success" onclick="rowModButt
 
 let fetchInitGet = {
     method: "GET",
-    headers: new Headers(),
+    headers: {
+        "authoirity": "ifksp-prod-probadocker-yr4i9t.mo5.mogenius.io",
+        "method": "GET",
+        "path": "/users",
+        "scheme": "https",
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "hu-HU,hu;q=0.9,en-US;q=0.8,en;q=0.7",
+        "cache-control": "max-age=0",
+        "dnt": 1
+        "sec-ch-ua": "Chromium";v="100", " Not A;Brand";v="99",
+        "sec-ch-ua-mobile": "?0"
+        "sec-ch-ua-platform": "Windows"
+        "sec-fetch-dest": "document"
+        "sec-fetch-mode": "navigate"
+        "sec-fetch-site": "none"
+        "sec-fetch-user": "?1"
+        "upgrade-insecure-requests": 1
+        "user-agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.162 Safari/537.36"
+        },
     mode: "cors",
     cache: "no-cache"
 };
